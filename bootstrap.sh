c807682ba05fd227df32a1aa7eca22b0994f28af
Dobri Danchev <12420863+danchev@users.noreply.github.com>
2022-05-11 00:41:01 -0500

    Add aws cli

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/bash
 
 #This script gets executed before codespaces default configuration
+
+# aws-cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install -i ~/.local/aws-cli -b ~/bin --update
+rm -rf awscliv2.zip aws
