4bff9f0ecf8ee9b01c1cc92982794f44c726e85f
Dobri Danchev <12420863+danchev@users.noreply.github.com>
2024-03-23 09:19:11 -0500

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,9 @@
 #This script gets executed before codespaces default configuration
 
 # pyenv
-sudo apt update; sudo apt install -y build-essential libssl-dev zlib1g-dev \
-libbz2-dev libreadline-dev libsqlite3-dev curl \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+# sudo apt update; sudo apt install -y build-essential libssl-dev zlib1g-dev \
+# libbz2-dev libreadline-dev libsqlite3-dev curl \
+# libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
 curl https://pyenv.run | bash
 
@@ -12,8 +12,7 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 
-. .bashrc && pyenv install 3.11.8 && pyenv global 3.11.8
-
+. .bashrc && pyenv install 3.12.2 && pyenv global 3.12.2
 
 # pipx
 python3 -m pip install --user pipx
